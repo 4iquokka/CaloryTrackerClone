171b133dd65e503b5dff74cd44f55ce084fa7cea
SHiNYJ <4iquokka@SHiNYJ-MBA13.local>
2023-01-22 00:10:48 +0900

    GenderScreen

--- app/src/main/java/ca/enzeroment/clone/calorytracker/MainActivity.kt
+++ app/src/main/java/ca/enzeroment/clone/calorytracker/MainActivity.kt
@@ -3,21 +3,17 @@ package ca.enzeroment.clone.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ca.enzeroment.clone.calorytracker.core.navigation.Route
 import ca.enzeroment.clone.calorytracker.navigation.navigate
+import ca.enzeroment.clone.calorytracker.onboarding.presentation.gender.GenderScreen
 import ca.enzeroment.clone.calorytracker.onboarding.presentation.welcome.WelcomeScreen
 import ca.enzeroment.clone.calorytracker.ui.theme.CaloryTrackerCloneTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +26,12 @@ class MainActivity : ComponentActivity() {
                 ) {
                     composable(Route.WELCOME){
                         WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.AGE){
+                        WelcomeScreen(onNavigate = navController::navigate)
+                    }
+                    composable(Route.GENDER){
+                        GenderScreen(onNavigate = navController::navigate)
                     }
 
                 }

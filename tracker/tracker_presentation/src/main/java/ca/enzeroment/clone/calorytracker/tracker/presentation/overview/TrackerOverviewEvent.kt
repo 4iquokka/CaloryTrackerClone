7ab6d90d007b572e2b16e82adecd4b362352f8f3
SHiNYJ <4iquokka@SHiNYJ-MBA13.local>
2023-01-24 15:17:47 +0900

    TrackerOverviewViewModel

---
+++ tracker/tracker_presentation/src/main/java/ca/enzeroment/clone/calorytracker/tracker/presentation/overview/TrackerOverviewEvent.kt
@@ -0,0 +1,11 @@
+package ca.enzeroment.clone.calorytracker.tracker.presentation.overview
+
+import ca.enzeroment.clone.calorytracker.tracker.domain.model.TrackedFood
+
+sealed class TrackerOverviewEvent(){
+    object OnNextDayClick: TrackerOverviewEvent()
+    object OnPreviousDayClick : TrackerOverviewEvent()
+    data class OnToggleMealClick(val meal : Meal) : TrackerOverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood) : TrackerOverviewEvent()
+    data class OnAddFoodClick(val meal : Meal) : TrackerOverviewEvent()
+}
